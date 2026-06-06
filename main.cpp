523d114678a2f9fe465abac02fd201688ffdee3d
Arsorta37 <andres1210.alp@gmail.com>
2026-06-06 05:41:17 +0200

    Refactor music player with configuration options

--- main.cpp
+++ main.cpp
@@ -7,31 +7,21 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include "reproductor.hpp"
 
-// Convierte un string a un float con un valor por defecto si falla
-float toFloat(const std::string& s, float defecto = 1.0f) {
-    try { return std::stof(s); }
-    catch (...) { return defecto; }
-}
-
-// Convierte un string a un int con un valor por defecto si falla
-int toInt(const std::string& s, int defecto = 1) {
-    try { return std::stoi(s); }
-    catch (...) { return defecto; }
-}
+const std::string FICHERO_OPCIONES = "opciones.txt";
 
 struct Album {
     std::string nombre;
     unsigned num_canciones;
 };
 
-// Muestra ppor pantalla los albums de la carpeta destino junto con su cantidad de canciones,
+// Muestra por pantalla los albums de la carpeta destino junto con su cantidad de canciones,
 // y devuelve el número de albums de la carpeta. En caso de error, devuelve 0.
 unsigned mostrarAlbumsEnCarpeta(const std::string& r) {
     // Buscamos los archivos
     unsigned contador = 0;
     std::vector<Album> albums;
     albums.clear();
-    std::cout << "\033[2K\r" << "Buscando albums en " << MAGENTA << r << RESET << "... ";
+    std::cout << "\033[2K\r" << "Buscando albums en " << color_Carpeta << r << RESET << "... ";
     try {
         for (const std::filesystem::directory_entry& entry : std::filesystem::directory_iterator(r)) {
             if (entry.is_regular_file()) {
@@ -57,27 +47,44 @@ unsigned mostrarAlbumsEnCarpeta(const std::string& r) {
     } catch (...) { std::cout << "error al abrir el directorio" << std::endl; return 0;}
 
     if (contador == 0) { // Comprobamos la búsqueda
-        std::cout << "No se ha encontrado ninguna cancion en " << MAGENTA << r << RESET << std::endl;
+        std::cout << "No se ha encontrado ninguna cancion en " << color_Carpeta << r << RESET << std::endl;
         return 0;
     } else if (albums.size() == 0) {
-        std::cout << "No se ha encontrado ningun album en " << MAGENTA << r << RESET << std::endl;
+        std::cout << "No se ha encontrado ningun album en " << color_Carpeta << r << RESET << std::endl;
         return 0;
     }
 
     std::cout << std::endl;
     for (size_t i=0; i < albums.size(); i++)
-        std::cout << CYAN << albums[i].nombre << RESET << " - " << AMARILLO << albums[i].num_canciones << RESET << " canciones" << std::endl;
+        std::cout << color_Album << albums[i].nombre << RESET << " - " << color_Numeros << albums[i].num_canciones << RESET << " canciones" << std::endl;
     return albums.size();
 }
 
 // Muestra por pantalla los controles
 void mostrarControles() {
-    std::cout << "Comandos (Esta ayuda se muestra con 'H' o \"help\"):\n"
-        << "Espacio: Play/Pause  | 0: Salir    | +/-/= [%]: Cambiar \% de volumen       | Clear: Limpiar terminal\n"
-        << "Q [sec]: Echar atras | W: Autoplay | E [sec]: Adelantar  | I: Ir a cancion | R: Reiniciar  | P: Playlist\n"
-        << "A [num]: Anterior    | S: Shuffle  | D [num]: Siguiente  | F: Explorador   | L: Loop\n"
-        << "Z / X: Por decidir   | B: Buscar   | V [num]: Velocidad  | M: Mostrar canciones\n"
-        << "C: Cargar carpeta    | C -r: Reiniciar reproductor       | C -a: Cargar solo un album" << std::endl;
+    std::cout << "Comandos (Esta ayuda se muestra con '" << controles_help.controlesToString() << "'):\n| "
+        << "Espacio" << (!controles_pause.controles.empty() ? "/" + controles_pause.controlesToString() : "") << ": Play/Pause     | " 
+        << controles_salir.controlesToString()      << ": Salir          | "
+        << controles_clear.controlesToString()      << ": Limpiar\n| "
+        << controles_vol_mas.controlesToString()    << " [%]: Aumentar volumen | "
+        << controles_vol_igual.controlesToString()  << " [%]: Igualar volumen | "
+        << controles_vol_menos.controlesToString()  << " [%]: Disminuir volumen\n| "
+        << controles_atras.controlesToString()      << " [sec]: Echar atras    | "
+        << controles_adelante.controlesToString()   << " [sec]: Adelantar     | "
+        << controles_reiniciar.controlesToString()  << ": Reiniciar\n| "
+        << controles_loop.controlesToString()       << ": Loop                 | "
+        << controles_autoplay.controlesToString()   << ": Autoplay            | "
+        << controles_shuffle.controlesToString()    << ": Shuffle\n| "
+        << controles_anterior.controlesToString()   << " [num]: Anterior       | "
+        << controles_siguiente.controlesToString()  << " [num]: Siguiente     | "
+        << controles_ir_a.controlesToString()       << ": Ir a cancion\n| "
+        << controles_explorador.controlesToString() << ": Explorador           | "
+        << controles_buscar.controlesToString()     << ": Buscar              | "
+        << controles_mostrar_canciones.controlesToString() << ": Mostrar canciones\n| "
+        << controles_velocidad.controlesToString()  << " [num]: Velocidad      |\n| "
+        << controles_cargar_carpeta.controlesToString() << ": Cargar carpeta  | "
+        << controles_cargar_carpeta.controlesToString() << " -" << flag_cargar_carpeta_reiniciar << ": Reiniciar reproductor | "
+        << controles_cargar_carpeta.controlesToString() << " -" << flag_cargar_carpeta_album << ": Cargar solo un album\n\n" << std::endl;
 }
 
 // Programa reproductor de música dentro de una carpeta
@@ -88,13 +95,30 @@ int main() {
     int indiceCancion = 0;
     std::string linea, nuevo_prompt, prompt_actual = "> ", ruta;
 
+    // Leemos el archivo de configuración
+    controlesPorDefecto(); // Primero inicializamos los controles con valores por defecto
+    if (!cargarOpciones(FICHERO_OPCIONES))
+        std::cout << color_Error << "Error" << RESET << ": No se ha podido abrir el fichero de configuraciones \""
+                  << color_Error << FICHERO_OPCIONES << RESET << "\"\n"
+                  << "Se aplicaran las configuraciones por defecto" << std::endl;
+
     // Preparamos la thread encargada de leer el teclado
     std::queue<std::string> cola_comandos;
     std::mutex mutex_cola;
-    std::thread input_thread(leerComandos, std::ref(cola_comandos), std::ref(mutex_cola));
+    std::thread input_thread(leerComandos, std::ref(cola_comandos), std::ref(mutex_cola), ruta_inicial.empty());
     input_thread.detach();
 
+    // Iniciamos la UI del usuario
     iniciarUI();
+    mostrarControles();
+    player.asignarVolumen(volumen_inicial);
+
+    // Cargamos la ruta inicial si se configuró
+    if (!ruta_inicial.empty()) {
+        player.cargarCarpeta(ruta_inicial, true);
+        ruta = ruta_inicial;
+    }
+
     while (true) {
         // Comprobar si acabó la canción
         if (player.haTerminado() && autoplay) player.reproducirSiguiente();
@@ -104,19 +128,19 @@ int main() {
           while (!cola_comandos.empty()) {
             std::string linea = cola_comandos.front();
             cola_comandos.pop();
-            if (linea.empty()) continue;
+            if (linea.empty()) { player.togglePausa(); continue; }
 
             if (esperando_carpeta) { // Introducir la ruta de una carpeta con 'C'
                 Comando carpeta = parsearComando(linea);
-                if (carpeta.accion == "0") {
-                    std::cout << "\033[2K\r" << "Se ha cancelado el cargar carpeta" << std::endl;
+                if (controles_salir.controlCorrecto(carpeta.accion)) {
+                    std::cout << "\033[2K\r" << "Se ha cancelado cargar carpeta" << std::endl;
                     esperando_carpeta = false;
                     cambiar_prompt = true;
                     nuevo_prompt = "> ";
                 } else {
                     if (esperando_album) {
                         if (mostrarAlbumsEnCarpeta(linea) != 0) {
-                            std::cout << "Seleccione un album ('0' para cancelar)" << std::endl;
+                            std::cout << "Seleccione un album ('" << controles_salir.controlesToString() << "' para cancelar)" << std::endl;
                             esperando_carpeta = false;
                             cambiar_prompt = true;
                             ruta = linea;
@@ -135,8 +159,8 @@ int main() {
                 continue;
             } else if (esperando_album) { // Introducir el album de una carpeta con "C -a"
                 Comando album = parsearComando(linea);
-                if (album.accion == "0") {
-                    std::cout << "\033[2K\r" << "Se ha cancelado el seleccionar album" << std::endl;
+                if (controles_salir.controlCorrecto(album.accion)) {
+                    std::cout << "\033[2K\r" << "Se ha cancelado seleccionar album" << std::endl;
                     esperando_album = false;
                     cambiar_prompt = true;
                     nuevo_prompt = "> ";
@@ -153,9 +177,9 @@ int main() {
             } else if (esperando_busqueda) { // Búsqueda de 'B' - Buscar
                 indiceCancion = player.buscarCancion(linea);
                 if (indiceCancion != -1) {
-                        std::cout << "\033[2K\r" << "Quieres reproducir la cancion "
-                         << VERDE << player.tituloIndice(indiceCancion) << RESET << "? [y/n]";
-                        esperando_confirmacion_busqueda = true;
+                    std::cout << "\033[2K\r" << "Quieres reproducir la cancion "
+                     << color_Cancion << player.tituloIndice(indiceCancion) << RESET << "? [y/n]" << std::flush;
+                    esperando_confirmacion_busqueda = true;
                 }
                 esperando_busqueda = false;
                 cambiar_prompt = true;
@@ -169,11 +193,11 @@ int main() {
                     esperando_confirmacion_busqueda = false;
                 } else if (confirmacion.accion == "n" || confirmacion.accion == "no") {
                     std::cout << "\033[2K\r" << "No se ha reproducido "
-                     << VERDE << player.tituloIndice(indiceCancion) << RESET << std::endl;
+                     << color_Cancion << player.tituloIndice(indiceCancion) << RESET << std::endl;
                     esperando_confirmacion_busqueda = false;
                 }
                 continue;
-            } else if (esperando_seleccion) { // selección de una opción de 'F' - Explorar
+            } else if (esperando_seleccion) { // Selección de una opción de 'F' - Explorar
                 if (player.seleccionarExplorador(linea)) esperando_seleccion = false;
                 continue;
             }
@@ -185,48 +209,43 @@ int main() {
             std::transform(c.begin(), c.end(), c.begin(), ::tolower); // Lo hacemos minúscula
 
             // Ejecutamos el comando
-            if (c == "") player.togglePausa();
-            else if (c == "a") player.reproducirRelativoNegativo(tieneValor ? toInt(cmd.valor) : 1);
-            else if (c == "d") player.reproducirRelativoPositivo(tieneValor ? toInt(cmd.valor) : 1);
-            else if (c == "e") player.saltar(tieneValor ? toFloat(cmd.valor) : 10.0f);
-            else if (c == "q") player.saltar(tieneValor ? -toFloat(cmd.valor) : -10.0f);
-            else if (c == "i" || c == "ir") player.reproducirIndice(tieneValor ? toInt(cmd.valor, -1) -1 : -1);
-            else if (c == "=") player.asignarVolumen(tieneValor ? toFloat(cmd.valor, 50.0f)/100 : 1.0f);
-            else if (c == "+") player.cambiarVolumen(tieneValor ? toFloat(cmd.valor, 10.0f)/100 : 0.1f);
-            else if (c == "-") player.cambiarVolumen(tieneValor ? -toFloat(cmd.valor, 10.0f)/100 : -0.1f);   
-            else if (c == "clear" || c == "cls" || c == "clean" || c == "cl") std::cout << "\033[2J\033[1;1H" << std::flush;
-            else if (c == "h" || c == "help") mostrarControles();
-            else if (c == "m") player.mostrarCanciones();
-            else if (c == "s") player.toggleShuffle();
-            else if (c == "l") player.toggleLoop();
-            else if (c == "r") player.reiniciar();
-            else if (c == "v") player.cambiarPitch(tieneValor ? toFloat(cmd.valor) : 1.0f);
-            else if (c == "p") std::cout << "Comando en desarrollo (Por decidir)" << std::endl;
-            else if (c == "t") std::cout << "Comando en desarrollo (Mostrar artistas)" << std::endl;
-            else if (c == "g") std::cout << "Comando en desarrollo (Comando por decidir)" << std::endl;
-            else if (c == "j") std::cout << "Comando en desarrollo (Comando por decidir)" << std::endl;
-            else if (c == "k") std::cout << "Comando en desarrollo (Comando por decidir)" << std::endl;
-            else if (c == "w") {
+                 if (controles_pause.controlCorrecto(c)) player.togglePausa(); // Espacio/Enter -> Pausa
+            else if (controles_anterior.controlCorrecto(c)) player.reproducirRelativoNegativo(tieneValor ? toInt(cmd.valor) : 1);
+            else if (controles_siguiente.controlCorrecto(c)) player.reproducirRelativoPositivo(tieneValor ? toInt(cmd.valor) : 1);
+            else if (controles_adelante.controlCorrecto(c)) player.saltar(tieneValor ? toFloat(cmd.valor) : 10.0f);
+            else if (controles_atras.controlCorrecto(c)) player.saltar(tieneValor ? -toFloat(cmd.valor) : -10.0f);
+            else if (controles_ir_a.controlCorrecto(c)) player.reproducirIndice(tieneValor ? toInt(cmd.valor, -1) - 1 : -1);
+            else if (controles_vol_igual.controlCorrecto(c)) player.asignarVolumen(tieneValor ? toFloat(cmd.valor, 50.0f) / 100 : 1.0f);
+            else if (controles_vol_mas.controlCorrecto(c)) player.cambiarVolumen(tieneValor ? toFloat(cmd.valor, 10.0f) / 100 : 0.1f);
+            else if (controles_vol_menos.controlCorrecto(c)) player.cambiarVolumen(tieneValor ? -toFloat(cmd.valor, 10.0f) / 100 : -0.1f);
+            else if (controles_clear.controlCorrecto(c)) reiniciarAreaScroll();
+            else if (controles_help.controlCorrecto(c)) mostrarControles();
+            else if (controles_mostrar_canciones.controlCorrecto(c)) player.mostrarCanciones();
+            else if (controles_shuffle.controlCorrecto(c)) player.toggleShuffle();
+            else if (controles_loop.controlCorrecto(c)) player.toggleLoop();
+            else if (controles_reiniciar.controlCorrecto(c)) player.reiniciar();
+            else if (controles_velocidad.controlCorrecto(c)) player.cambiarPitch(tieneValor ? toFloat(cmd.valor) : 1.0f);
+            else if (controles_autoplay.controlCorrecto(c)) {
                 if (autoplay) std::cout << "Reproduccion automatica desactivada" << std::endl;
-                    else      std::cout << "Reproduccion automatica activada" << std::endl;
+                else          std::cout << "Reproduccion automatica activada" << std::endl;
                 autoplay = !autoplay;
             }
-            else if (c == "f") {
+            else if (controles_explorador.controlCorrecto(c)) {
                 player.iniciarExplorador(tieneValor ? cmd.valor : "");
                 esperando_seleccion = true;
             }
-            else if (c == "c") {
+            else if (controles_cargar_carpeta.controlCorrecto(c)) {
                 reiniciar_reproductor = cmd.tieneFlag('r');
                 if (cmd.tieneFlag('a')) esperando_album = true;
                 if (!tieneValor || cmd.valor == "") {
-                    std::cout << "Escriba el directorio donde estan sus canciones ('0' para cancelar)";
+                    std::cout << "Escriba el directorio donde estan sus canciones ('" << controles_salir.controlesToString() << "' para cancelar)" << std::endl;
                     cambiar_prompt = true;
                     esperando_carpeta = true;
                     nuevo_prompt = "Directorio donde estan las canciones: ";
                 } else {
                     if (esperando_album) {
                         if (mostrarAlbumsEnCarpeta(cmd.valor) != 0) {
-                            std::cout << "Seleccione un album ('0' para cancelar)" << std::endl;
+                            std::cout << "Seleccione un album ('" << controles_salir.controlesToString() << "' para cancelar)" << std::endl;
                             ruta = cmd.valor;
                             cambiar_prompt = true;
                             nuevo_prompt = "Album que cargar: ";
@@ -234,31 +253,37 @@ int main() {
                     } else player.cargarCarpeta(cmd.valor, reiniciar_reproductor);
                 }
             }
-            else if (c == "b") {
+            else if (controles_buscar.controlCorrecto(c)) {
                 if (tieneValor) {
                     indiceCancion = player.buscarCancion(cmd.valor);
                     if (indiceCancion != -1) {
                         std::cout << "\033[2K\r" << "Quieres reproducir la cancion "
-                         << VERDE << player.tituloIndice(indiceCancion) << RESET << "? [y/n]";
+                         << color_Cancion << player.tituloIndice(indiceCancion) << RESET << "? [y/n]" << std::flush;
                         esperando_confirmacion_busqueda = true;
                     }
                 } else {
-                    std::cout << "Escriba el titulo de la cancion que quiera buscar" << std::endl;
+                    std::cout << "Escriba el titulo de la cancion que quiera buscar" << std::flush;
                     cambiar_prompt = true;
                     nuevo_prompt = "Buscar: ";
                     esperando_busqueda = true;
                 }
             }
-            else if (c == "0") {
+            else if (controles_salir.controlCorrecto(c)) {
+                player.pausar();
+                borrarUI();
+                reiniciarAreaScroll();
                 #ifndef _WIN32
                     restaurarTerminal();
                 #endif
-                player.pausar();
-                std::cout << "Cerrando el programa..." << std::endl;
                 exit(0);
             }
+            else if (c == "p") std::cout << "Comando en desarrollo (Por decidir)"      << std::endl;
+            else if (c == "t") std::cout << "Comando en desarrollo (Mostrar artistas)" << std::endl;
+            else if (c == "g") std::cout << "Comando en desarrollo (Por decidir)"      << std::endl;
+            else if (c == "j") std::cout << "Comando en desarrollo (Por decidir)"      << std::endl;
+            else if (c == "k") std::cout << "Comando en desarrollo (Por decidir)"      << std::endl;
             else std::cout << "Comando desconocido: " << c << std::endl;
-          }
+            }
         }
 
         if (cambiar_prompt) { // Actualizamos el prompt
@@ -270,7 +295,7 @@ int main() {
         { // Actualizamos la UI
             auto [pos, dur] = player.obtenerPosicionYDuracion();
             std::lock_guard<std::mutex> lock(mutexPrompt);
-            actualizarUI(player.obtenerInfoActual(), pos, dur, prompt_actual, lineaActual);
+            actualizarUI(player.obtenerInfoActual(), pos, dur, prompt_actual, input_actual);
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(50)); // Dormimos para no consumir la CPU al 100%
